52e41e479ac6bdfd18af938557ac4db118badb80
Dawit Gemeda <dawitgemeda@The-Hidden-Genius-Project-13.local>
2022-10-19 18:27:47 -0700

    first commit

---
+++ ai_bot_chat.py
@@ -0,0 +1,8 @@
+def ai_bot_chat():
+  command= input("AI: How's your day?\n")
+  command= input("AI: Oh I see. What is something that could make your day better?\n")
+  command= input("AI: If I can grant you that what will I get in return?\n")
+  command= input("AI: Well it was nice meeting you bye!\n")
+
+ 
+ai_bot_chat()
